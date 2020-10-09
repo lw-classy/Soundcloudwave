f3551184ee16b368b9db68462d84faa0288e127e
lw-classy <levinwalser@gmx.de>
2020-10-09 18:30:37 +0200

    Initial Commit

---
+++ SoundCloudWave/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SoundCloudWave
+//
+//  Created by LevinJungstier on 09.10.20.
+//  Copyright © 2020 LevinJungstier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .blue // TODO: Proof we have loaded
+    }
+
+
+}
+
